794ae780c153574e9dfcc9cabce418e193128b7e
BenMacklin <benjaminmacklin92@gmail.com>
2022-11-14 22:24:55 -0500

    Comments added, db migrated and validations created

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to user
+  belongs_to :user
+  has_many :comments
+
+  validates :body, presence: true
+  validates :title, length:{ minimum: 3 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many posts
+
+  has_many :posts
+  has_many :comments
 
   validates :username, length:{ minimum: 2 }
   validates :username, presence: true
